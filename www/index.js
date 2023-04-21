8884d7e32d0c838f45ce7d2bf51b375d755fa53c
David Giannikos <david.giannikos@gmail.com>
2023-04-21 14:33:19 +0200

    get canvas; draw world

--- www/index.js
+++ www/index.js
@@ -1,7 +1,33 @@
 import init, { World } from "snake_game";
 
 init().then(_ => {
+    const cellSize = 20;    // 20px
+    // World
     const world = World.new();
-    console.log(world.get_width());
+    const worldWidth = world.get_width();
+    // Canvas
+    const canvas = document.getElementById("snake-canvas");
+    const ctx = canvas.getContext("2d");
+
+    canvas.height = worldWidth * cellSize;  // = 16*10px ->16px
+    canvas.width = worldWidth * cellSize;
+
+    function drawWorld() {
+        ctx.beginPath();
+
+        for (let x=0; x < worldWidth+1; x++) {
+            ctx.moveTo(cellSize * x, 0);
+            ctx.lineTo(cellSize * x, worldWidth * cellSize);
+        }
+
+        for (let y=0; y < worldWidth+1; y++) {
+            ctx.moveTo(0, cellSize * y);
+            ctx.lineTo(worldWidth * cellSize, cellSize * y);
+        }
+
+        ctx.stroke();
+    }
+
+    drawWorld();
 })
 
